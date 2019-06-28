5fb7e3611bd9448b2d0cc7d931849f35f6059fbf
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-28 16:38:42 +0100

    Initial setup and fetch of films working.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Hello Mike</h1>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      filmsArray: [],
+    }
+  },
+  mounted() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+    .then(res => res.json())
+    .then(data => this.filmsArray = data);
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
