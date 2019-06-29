64d2d371d68a0da00fe18c73c3eab723a5afa2fd
Michael Urquhart <michaelurquhart@hotmail.com>
2019-06-29 14:18:23 +0100

    Added new component for empty watched film list.

---
+++ src/components/NoWatchedFilms.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+    <h2>You have no films in your watched film list</h2>
+    <p>Why not add some <router-link :to="{ name: 'film-view'}">here?</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'no-watched-films'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/WatchedFilmView.vue
+++ src/views/WatchedFilmView.vue
@@ -1,17 +1,25 @@
 <template lang="html">
   <div>
     <h2>List of all Studio Ghibli films you've seen</h2>
+    <no-watched-films v-if="noWatched"></no-watched-films>
     <film-list :films="watchedFilms"></film-list>
   </div>
 </template>
 
 <script>
 import FilmList from '@/components/FilmList'
+import NoWatchedFilms from '@/components/NoWatchedFilms'
 export default {
   name: 'watched-film-view',
   props: ['watchedFilms'],
   components: {
-    'film-list': FilmList
+    'film-list': FilmList,
+    'no-watched-films': NoWatchedFilms
+  },
+  computed: {
+    noWatched: function() {
+      return this.watchedFilms.length === 0
+    }
   }
 }
 </script>
